263d664b60817f124e74ba82bb43661d3ee8aa4e
vishavattri <vishavattricanada@gmail.com>
2024-12-02 01:49:18 -0500

    03 completed

---
+++ C++ basics/03 Sum of two numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+   int a,b ;
+   int sum ;
+
+    cout<<"Enter two numbers"<<endl;
+    cout<<"a=";
+    cin>> a;
+    cout<<"b=";
+    cin>> b; 
+    sum=a+b;
+    cout<<"Sum of two numbers is:"<<sum;
+    return 0;
+}
