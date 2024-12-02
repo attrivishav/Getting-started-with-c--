a17bb1e2870b87a22e8ac3a20642394692ff5c37
vishavattri <vishavattricanada@gmail.com>
2024-12-02 03:11:29 -0500

    05 completed

---
+++ C++ basics/05 Multiplication of two numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+   int a,b ;
+   int mult ;
+
+    cout<<"Enter two numbers"<<endl;
+    cout<<"a=";
+    cin>> a;
+    cout<<"b=";
+    cin>> b; 
+    mult=a*b;
+    cout<<"Multiplication of two numbers is:"<<mult;
+    return 0;
+}
