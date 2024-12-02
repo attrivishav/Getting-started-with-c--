d98f2adeb86ec04302f5d9038bd29f8645aef58c
vishavattri <vishavattricanada@gmail.com>
2024-12-02 02:01:19 -0500

    04 done

---
+++ C++ basics/04 Subtraction of two numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std; 
+
+int main()
+{
+   int a,b ;
+   int sub ;
+
+    cout<<"Enter two numbers"<<endl;
+    cout<<"a=";
+    cin>> a;
+    cout<<"b=";
+    cin>> b; 
+    sub=a-b;
+    cout<<"Subtraction of two numbers is:"<<sub;
+    return 0;
+}
