44962e748eb251aaeb97b5af1db3e0eb0fb6009a
Vishav Attri <vishavattricanada@gmail.com>
2024-12-04 10:44:39 -0500

    division of two numbers and area of circle added,

---
+++ C++ basics/06 Division of two numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    double a ,b;
+    double d;
+    cout<<"Enter  value of a :";
+    cin>>a;
+    cout<<"Enter value of b :";
+    cin>>b;
+    d= a/b;
+    cout<<" After Division : "<<d;
+    return 0;
+}
+
---
+++ C++ basics/07 Area of circle.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double r;
+    double p =3.142, area;
+    cout<<"enter the radius of circle :";
+    cin>>r;
+    area=( p *(r*r));
+    cout<<"area of circle is:"<<area;
+    return 0;
+}
+
