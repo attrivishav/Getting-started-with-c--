4ddcc90b7cb8d69327bbcf06b5546ae32ed8bd5d
Vishav Attri <vishavattricanada@gmail.com>
2024-12-04 18:55:40 -0500

    area of triangle added

---
+++ C++ basics/08 area of triangle.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    double b , h;
+    double area;
+    cout<<"enter the base of trangle: ";
+    cin>>b;
+    cout<<"enter the hight of triangle: ";
+    cin>>h;
+    area = (0.5)*b*h;
+    cout<<"area of triangle is :"<<area;
+    return 0;
+}
